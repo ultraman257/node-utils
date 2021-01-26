199dd226a5d2bb832436732a547a0882d0a256b7
Matthew <git@matthewthaskins.com>
2021-01-26 23:28:25 +0000

    Removed debug console.log

--- utils/authentication.js
+++ utils/authentication.js
@@ -5,15 +5,10 @@ const {malformed} = require("./errorResponses");
 
 module.exports = function(req, res, next) {
 
-    console.log(req.headers);
 
     if(req.header("Authorization") === undefined) return malformed(res);
     if(req.header("Authorization").split(" ").length !== 2) return malformed(res);
     if(req.header("Authorization").split(" ")[0] !== "Bearer") return malformed(res);
-
-    console.log(req.header("Authorization").split(" ")[1]);
-
-    console.log(global.publicKey)
 
     let token = jwt.verify(req.header("Authorization").split(" ")[1], global.publicKey, { algorithms: ["RS256"]});
 
