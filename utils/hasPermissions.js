bc6804ba551bb73e14d2999bf927c6ca35f0db1e
Matthew <git@matthewthaskins.com>
2021-01-27 22:11:16 +0000

    Made hasPermission into middleware function

--- utils/hasPermissions.js
+++ utils/hasPermissions.js
@@ -1,12 +1,13 @@
-module.exports = function(req, res, permission) {
+module.exports = function(permission) {
+    return function(req, res, next) {
 
-    let permissions = req.user.permissions;
+        let permissions = req.user.permissions;
 
-    if(permissions.indexOf("*") === -1 && permissions.indexOf(permission) === -1) {
-        return res.status(403).json({ error: true, status: 403, message: "You do not have permission to access this resource"})
-    } else {
-        return true;
+        if(permissions.indexOf("*") !== -1 || permissions.indexOf(permission) !== -1) {
+            next();
+        } else {
+            return res.status(403).json({ error: true, status: 403, message: "You do not have permission to access this resource"});
+        }
+
     }
-
-
 }
